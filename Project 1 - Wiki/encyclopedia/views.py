90569d81c572c0e4123b3e95e1ae62b1c6248f58
PatRogala <patrykrogedu@gmail.com>
2021-05-31 13:17:16 +0200

    Add new page

--- Project 1 - Wiki/encyclopedia/views.py
+++ Project 1 - Wiki/encyclopedia/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from . import util
+from django import forms
 
 
 def index(request):
@@ -14,7 +15,9 @@ def entry(request, title):
       "entry": entry
     })
   else:
-    return render(request, "encyclopedia/error.html")
+    return render(request, "encyclopedia/error.html", {
+      "error": "not a valid page"
+    })
 
 def search(request):
   entries = []
@@ -24,3 +27,23 @@ def search(request):
   return render(request, "encyclopedia/search.html", {
     "entries": entries
   })
+
+class NewEntryForm(forms.Form):
+    title = forms.CharField(label="Title")
+    entry_body = forms.CharField(widget=forms.Textarea)
+
+def new(request):
+  if request.method == "GET":
+    return render(request, "encyclopedia/new.html", {
+      "form": NewEntryForm()
+    })
+  else:
+    title = request.POST['title']
+    body = request.POST['entry_body']
+    if util.get_entry(title):
+      return render(request, "encyclopedia/error.html", {
+        "error": "already exists"
+      })
+    else:
+      util.save_entry(title, body)
+      return redirect("entry", title=title)
